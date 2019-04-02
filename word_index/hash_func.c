85e83e4c40ecae0d9859ca520aef5e6fe8e8d448
Timo <leemanstimo@gmail.com>
2019-04-02 07:11:31 -0700

    Working on hash_tables

--- word_index/hash_func.c
+++ word_index/hash_func.c
@@ -2,6 +2,35 @@
 /* Do not edit this function, as it used in testing too
  * Add you own hash functions with different headers instead. */
 unsigned long hash_too_simple(unsigned char *str) {
+    
     return (unsigned long) *str;
 }
 
+// unsigned long hash(unsigned char *str) {
+    
+//     unsigned long hash = 5381;
+//     int c;
+
+//     while (c = *str++) {
+//         hash = ((hash << 5) + hash) + c;
+//     }
+
+//     return hash;
+// }
+
+
+
+// unsigned
+
+// unsigned long ElfHash ( const unsigned char *s )
+// {
+//     unsigned long   h = 0, high;
+//     while ( *s )
+//     {
+//         h = ( h << 4 ) + *s++;
+//         if ( high = h & 0xF0000000 )
+//             h ^= high >> 24;
+//         h &= ~high;
+//     }
+//     return h;
+// }
--- word_index/hash_table.c
+++ word_index/hash_table.c
@@ -5,6 +5,7 @@
 #include "hash_table.h"
 #include "array.h"
 
+/* Struct for the table*/
 struct table {
     // The (simple) array used to index the table
     struct node **array;
@@ -21,7 +22,7 @@ struct table {
 
 /* Note: This struct should be a *strong* hint to a specific type of hash table
  * You may implement other options, if you can build them in such a way they
- * pass all tests. However, the other options are generally harder to code. */
+ * pass all tests. However, the other options ares generally harder to code. */
 struct node {
     // The string of characters that is the key for this node
     char *key;
@@ -32,15 +33,81 @@ struct node {
     struct node *next;
 };
 
-// ... SOME CODE MISSING HERE ...
+struct node* node_init(char key, int value) {
 
-struct table *table_init(unsigned long capacity, double max_load,
-                            unsigned long (*hash_func)(unsigned char *)) {
-    // ... SOME CODE MISSING HERE ...
+    struct node *n = malloc(sizeof(struct node));
+    if (n == NULL) {
+        return NULL;
+    }
+    struct array *a = array_init(1);
+    if (a == NULL) {
+        return NULL;
+    }
+    n->value = a;
+    return n;
 }
 
+/* Clear up node and its pointers. Return 1 if it failed. */
+struct node *free_node(struct node* n) {
+
+    if (n == NULL) {
+        return 1;
+    }
+    free(n->value);
+    free(n);
+    return 0;
+}
+
+/* Create the struct table and assign pointers to it. Returns NULL if failed.*/
+struct table *table_init(unsigned long capacity, double max_load,
+                            unsigned long (*hash_func)(unsigned char *)) {
+    struct table *table = calloc(1, sizeof(struct table));
+    if (table == NULL) {
+        return NULL;
+    }
+    table->capacity = capacity;
+    table->load = 0;
+    table->max_load = max_load;
+    table->hash_func = hash_func;
+    table->array = calloc(capacity, sizeof(struct node));
+    if (table->array == NULL) {
+        return NULL;
+    }
+    return table;
+}
+
+int double_size(struct table *t) {
+
+    if (t == NULL) {
+        return 1;
+    }
+    struct node* new_table = calloc((t->capacity)* 2, sizeof(struct node));
+    if (new_table == NULL) {
+        return 1;
+    }
+    for (i = 0; i < t->capacity; i++) {
+
+        // Get first node
+        struct node* current = t->a[i];
+        while(current != NULL) {
+
+            struct node* next = current->next;
+            current->next = NULL;
+            current = next;
+        }
+
+    }
+}
+
+/* Insert node with key / value to the table. Return 1 if failed. */
 int table_insert(struct table *t, char *key, int value) {
-    // ... SOME CODE MISSING HERE ...
+    
+    if (t == NULL && key == NULL) {
+        return 1;
+    }
+
+
+
 }
 
 struct array *table_lookup(struct table *t, char *key) {
@@ -52,5 +119,7 @@ int table_delete(struct table *t, char *key) {
 }
 
 void table_cleanup(struct table *t) {
-    // ... SOME CODE MISSING HERE ...
+    
+    free(t->array);
+    free(t);
 }
